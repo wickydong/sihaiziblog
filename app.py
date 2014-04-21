a272fa4d078eee3785a71f77de12ddee52ad3f2b
Dong Kun <kdong@dnspod.com>
2014-04-21 18:16:16 +0800

    office

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 from flask import Flask,render_template,url_for,request
+import MySQLdb
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
