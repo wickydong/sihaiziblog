88f54bd6187d7d2b5074d9c828f27f83db71c49a
Dong Kun <kdong@dnspod.com>
2014-07-01 18:32:48 +0800

    create article and show index

--- app.py
+++ app.py
@@ -6,6 +6,7 @@
 
 from flask import Flask,render_template,url_for,request,session,redirect
 import MySQLdb
+from datetime import *
 from flaskext.mysql import MySQL
 import sys
 reload(sys)
@@ -20,11 +21,11 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.secret_key = "\xdd\xe6\x1c\x9b2K\x10I\x1d\x03\xed~\xdadr\xb21\xac!\x89\x0c\xc4\x15`"
 mysql.init_app(app)
 
-@app.route("/admin")
+@app.route("/admin")  #管理员后台
 def admin():
     return render_template("login.html")
 
-@app.route("/login",methods=["GET","POST"])
+@app.route("/login",methods=["GET","POST"]) #验证登陆，进入后台编辑
 def login():
     if request.method == "POST" or request.method == "GET":
         cursor = mysql.get_db().cursor()
@@ -35,24 +36,51 @@ def login():
         passwd = str(passwd)
         if request.form["user"] == user and request.form["passwd"] == passwd:
             session["user"] = user
-            return redirect(url_for("edit.html"))
+            return render_template("edit.html",user=user)
         else:
             return render_template("login.html",mess="failed")
         cursor.close()
 
-@app.route("/")
+@app.route("/logout") #退出后台
+def logout():
+    session.pop("user",None)
+    return redirect(url_for("index"))
+
+@app.route("/edit") #后台选择操作
+def edit():
+    cho = request.args.get("chose")
+    user = request.args.get("user")
+    if cho == "write":
+        return render_template("write.html",user=user)
+
+@app.route("/write",methods=["POST"])
+def write():
+    title = str(request.form["title"])
+    content = str(request.form["content"])
+    user = str(request.form["user"])
+    times = str(date.today())
+#    user = request.args.get("user")
+ #   print user
+    cursor = mysql.get_db().cursor()
+    cursor.execute("use sihaizi")
+    #cursor.execute("insert into blog(title,article,author,date) values(%s,%s,%s,%s)" %(title,content,user,times))
+    sql = "insert into blog(title,article,author,date) values('%s','%s','%s','%s')" %(title,content,user,times)
+    cursor.execute(sql)
+    mysql.get_db().commit()
+    cursor.close()
+    return redirect(url_for(index))
+@app.route("/")  #首页
 def index():
     cursor = mysql.get_db().cursor()
     cursor.execute("use sihaizi")
     cursor.execute("select title,article,author,date from blog")
-    print cursor.fetchall()
-    return render_template("index.html")
-@app.route("/sx")
-def sx():
-    return render_template("sx.html")
+    mess = cursor.fetchmany(size=10)
+    #print type(mess)
+    return render_template("index.html",mess=mess)#,autoescape=True)
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080,debug=True)
