ff154f3cd0edf0213fb82b0c74ff1637f509437b
root <root@ip-172-31-8-169.ap-northeast-1.compute.internal>
2014-07-23 07:18:30 +0000

    xianshang

--- app.py
+++ app.py
@@ -96,9 +96,9 @@ def index():
         page = int(idsum) / 10 + 1
     return render_template("index.html",mess=mess,page=page)#,autoescape=True)
 
-@app.route("/sx")
-def sx():
-    return render_template("sx.html")
+@app.route("/links")
+def links():
+    return render_template("links.html")
 @app.route("/talk")
 def talk():
     title = str(request.args.get("title"))
@@ -112,4 +112,4 @@ def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8080,debug=True)
+    app.run(host="0.0.0.0",port=8080)
